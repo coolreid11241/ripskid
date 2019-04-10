4bcf38302b26ba5391e88fb2f1b150a2c5aa8370
oonqt <r0blox1053@outlook.com>
2019-04-09 18:31:34 -0700

    Implemented aliases, fixed amount of messages deleted by bulk delete

---
+++ commands/cmds.js
@@ -0,0 +1,9 @@
+module.exports.run = async (client, Discord, message, args) => {
+  
+}
+
+module.exports.help = {
+  name: "cmds",
+  elevated: false,
+  aliases: ["help", "commands"]
+}
--- commands/purge.js
+++ commands/purge.js
@@ -1,10 +1,12 @@
 module.exports.run = async (client, Discord, message, args) => {
   if(!args[0]) {
+    await message.delete();
     message.channel.bulkDelete(100);
     message.channel.send("Deleted 100 messages").then(msg => msg.delete(5000));
   } else {
     if(isNaN(args[0])) return message.channel.send(":x: Must be number.");
     if(args[0] > 100) return message.channel.send(":x: No more than 100 messages can be deleted at a time");
+    await message.delete();
     message.channel.bulkDelete(args[0]);
     message.channel.send(`Deleted ${args[0]} messages`).then(msg => msg.delete(5000));
   }
